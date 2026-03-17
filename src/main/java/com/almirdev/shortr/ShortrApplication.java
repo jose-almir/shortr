4a337bfb08cf8098bbc054b38ef89bb1d7eeacdc
jose-almir <jr.cod.dev@gmail.com>
2026-03-17 16:24:52 -0300

    feat: add application main entry point

---
+++ src/main/java/com/almirdev/shortr/ShortrApplication.java
@@ -0,0 +1,13 @@
+package com.almirdev.shortr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortrApplication.class, args);
+	}
+
+}
